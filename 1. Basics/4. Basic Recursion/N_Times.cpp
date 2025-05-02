aa6417bff867f654b158d2654016498bb493f65d
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-05-03 00:30:52 +0600

    Create N_Times.cpp

---
+++ 1. Basics/4. Basic Recursion/N_Times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void func(int i, int n)
+{
+    if(i>n) return;
+    cout<<"Jim"<<endl;
+    func(i+1,n);
+}
+
+int main()
+{
+    int n = 4;
+    func(1,n);
+    return 0;
+}
