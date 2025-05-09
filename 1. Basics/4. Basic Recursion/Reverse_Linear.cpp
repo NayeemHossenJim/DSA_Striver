a7c9cf5e82845768efcbeaa8598dda521bf7b970
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-05-09 23:09:35 +0600

    Update

---
+++ 1. Basics/4. Basic Recursion/Reverse_Linear.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void func(int i, int n)
+{
+    if(i<1) return;
+    cout<<i <<endl;
+    func(i-1,n);
+}
+
+
+int main()
+{
+    int n = 4;
+    func(n,n);
+    return 0;
+}
